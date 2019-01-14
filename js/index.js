f29822ea24562eecbd7d49d8bcd4422f5eb8ac67
David Edson <eddo888@tpg.com.au>
2019-01-14 23:14:55 +1100

    tabs

--- js/index.js
+++ js/index.js
@@ -1,66 +1,66 @@
 var xml=`\
 <?xml version="1.0" encoding="UTF-8"?>
 <eddo:root xmlns:eddo="ns:eddo" attr='val'>
-    <child1>text1</child1>
-    <!-- you cant see me -->
-    <child2>text2</child2>
+	<child1>text1</child1>
+	<!-- you cant see me -->
+	<child2>text2</child2>
 </eddo:root>
 `;
 
 var obj = null;
 
 function reload(){
-    var igfun = $('#igfunchk').is(":checked");
-    $('#mygoodies').find('*').remove();
-    $('#mygoodies').expando(obj,igfun);
-    var now = new Date();
-    $('#when').text(now.toISOString().replace("T"," ").replace("Z",""));
-    $('#igfun').text(igfun);
+	var igfun = $('#igfunchk').is(":checked");
+	$('#mygoodies').find('*').remove();
+	$('#mygoodies').expando(obj,igfun);
+	var now = new Date();
+	$('#when').text(now.toISOString().replace("T"," ").replace("Z",""));
+	$('#igfun').text(igfun);
 }
 
 function parse(){
-    if ($('#is-json').is(':checked')) {
-        obj = JSON.parse($('#source').val());
-    } 
-    if ($('#is-xml').is(':checked')) {
-        obj = fromXML($('#source').val());
-    }
-    reload();
+	if ($('#is-json').is(':checked')) {
+		obj = JSON.parse($('#source').val());
+	} 
+	if ($('#is-xml').is(':checked')) {
+		obj = fromXML($('#source').val());
+	}
+	reload();
 }
 
 function unparse(){
-    if ($('#is-json').is(':checked')) {
-        $('#source').val(JSON.stringify(obj,null,4));
-    } 
-    if ($('#is-xml').is(':checked')) {
-        $('#source').val(toXML(obj,null,4));
-    }
+	if ($('#is-json').is(':checked')) {
+		$('#source').val(JSON.stringify(obj,null,4));
+	} 
+	if ($('#is-xml').is(':checked')) {
+		$('#source').val(toXML(obj,null,4));
+	}
 }
 
 $(document).ready(function() {
 
-    $('input').checkboxradio();
-    
-    $('#igfunchk').change(reload);
-    
-    $('#diagnostics').hide();
-    $('#diagchk').change(function(){
-        if ($(this).is(':checked')) {
-            $('#diagnostics').show();
-        } 
-        else {
-            $('#diagnostics').hide();
-        }
-    });
+	$('input').checkboxradio();
+	
+	$('#igfunchk').change(reload);
+	
+	$('#diagnostics').hide();
+	$('#diagchk').change(function(){
+		if ($(this).is(':checked')) {
+			$('#diagnostics').show();
+		} 
+		else {
+			$('#diagnostics').hide();
+		}
+	});
 
-    $('#parse').button().click(parse);
-    $('#unparse').button().click(unparse);
-    
-    $('#source').val(xml);
-    parse();
-    
-    $('#is-json').change(unparse);
-    $('#is-xml').change(unparse);
+	$('#parse').button().click(parse);
+	$('#unparse').button().click(unparse);
+	
+	$('#source').val(xml);
+	parse();
+	
+	$('#is-json').change(unparse);
+	$('#is-xml').change(unparse);
 
 });
 
